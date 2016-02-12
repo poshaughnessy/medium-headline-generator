ed943d2168d7718c8611417b7544372dc58e94f4
Peter O'Shaughnessy <peter.oshaughnessy@pebblecode.com>
2016-02-12 17:28:40 +0000

    Add Why You're Wrong About...

--- src/js/headlineGenerator.js
+++ src/js/headlineGenerator.js
@@ -4,11 +4,11 @@ import {chooseOneAtRandom} from './utils';
  * More headline styles coming soon!
  */
 const HEADLINE_STYLES = {
-  FORCED_CULTURAL_REFERENCE: 'FORCED_CULTURAL_REFERENCE'
+  FORCED_CULTURAL_REFERENCE: 'FORCED_CULTURAL_REFERENCE',
+  WHY_YOURE_WRONG_ABOUT: 'WHY_YOURE_WRONG_ABOUT'
+  //AND_WHY_IT_MATTERS: 'AND_WHY_IT_MATTERS'
   //QTWTAIN: 'QTWTAIN',
-  //THIS_IS_WHAT_HAPPENS: 'THIS_IS_WHAT_HAPPENS',
-  //AND_WHY_IT_MATTERS: 'AND_WHY_IT_MATTERS',
-  //WHY_YOURE_WRONG_ABOUT: 'WHY_YOURE_WRONG_ABOUT'
+  //THIS_IS_WHAT_HAPPENS: 'THIS_IS_WHAT_HAPPENS'
 };
 
 const SHORT_PITHY_QUOTES = [
@@ -18,7 +18,6 @@ const SHORT_PITHY_QUOTES = [
   'Fail Fast',
   'Success Is Built On Failure',
   'Risking Nothing Risks Everything',
-  'To Dare Is To Do',
   'Innovate Or Die',
   'If You Change Nothing, Nothing Will Change',
   'Don\'t Dream Of Success, Work For It',
@@ -30,14 +29,15 @@ const TRENDY_CULTURAL_REFERENES = [
   'The Martian',
   'The Big Short',
   'Deadpool',
-  'Batman v Superman Dawn of Justice',
+  'Batman v Superman',
   'Zoolander 2',
   'Star Trek Beyond',
   'The New Indepenedence Day Movie',
   'The New Jason Bourne Movie',
   'Justin Bieber',
   'Taylor Swift',
-  'Kanye West'
+  'Kanye West',
+  'The Gravitational Wave Discovery'
 ];
 
 const BUSINESS_BUZZ_WORDS = [
@@ -48,6 +48,15 @@ const BUSINESS_BUZZ_WORDS = [
   'Industry Disruption',
   'Lean Startup',
   'Agile Software Development'
+];
+
+const CONTENTIOUS_THEMES = [
+  'The Death of Twitter',
+  'The Impending Robot Apocalypse',
+  'Google\'s Tax Policy',
+  'Silicon Valley Excess',
+  'Donald Trump',
+  'The Death of Privacy'
 ];
 
 export function generateHeadline() {
@@ -66,6 +75,15 @@ export function generateHeadline() {
       const businessBuzzWord = chooseOneAtRandom(BUSINESS_BUZZ_WORDS);
 
       return `${shortPithyQuote}: What ${trendyCulturalReference} Teaches Us About ${businessBuzzWord}`;
+
+    /**
+     * Why You're Wrong About $contentiousTheme
+     */
+    case HEADLINE_STYLES.WHY_YOURE_WRONG_ABOUT:
+
+      const contentiousTheme = chooseOneAtRandom(CONTENTIOUS_THEMES);
+
+      return `Why You're Wrong About ${contentiousTheme}`;
 
   }
 
