8f2fe0b979856ac59868729584b01570fab51cc2
O'Shaughnessy, Peter <Peter.OShaughnessy@pearson.com>
2016-02-13 00:31:55 +0000

    Copy change, add favicon, reformat code

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 'use strict';
 
 const express = require('express'),
-    PORT = process.env.PORT || 9000;
+  PORT = process.env.PORT || 9000;
 
 let app = express();
 
-app.use( '/bundle.js', express.static('build/bundle.js') );
-app.use( '/css', express.static('public/css') );
+app.use('/bundle.js', express.static('build/bundle.js'));
+app.use('/css', express.static('public/css'));
+app.use('/images', express.static('public/images'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.use((req, res) => {
-    res.status(404).send('Page not found');
+  res.status(404).send('Page not found');
 });
 
 app.listen(PORT);
